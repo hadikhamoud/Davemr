99d4024fc4a024a46fba9d09b427704d5eeb932e
Hadi Hamoud <hadihamoud@Hadis-MacBook-Pro.local>
2022-05-02 11:56:46 +0300

    heroku push

--- app.py
+++ app.py
@@ -305,9 +305,6 @@ def reset():
 def serve():
     return send_from_directory(app.static_folder,'index.html')
 
-@app.errorhandler(404)
-def not_found(e):
-    return send_from_directory(app.static_folder,'index.html')
 
 if __name__ == "__main__":
      app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
