60c73cdfb1e6cf3802b20e14b5dc345fe735317d
Hadi Hamoud <hadihamoud@Hadis-MacBook-Pro.local>
2022-05-02 11:08:37 +0300

    connect flask react

--- app.py
+++ app.py
@@ -302,5 +302,9 @@ def reset():
 
 # CORS(app)
 @app.route('/')
-def server():
+def serve():
     return send_from_directory(app.static_folder,'index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
