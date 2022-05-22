daa7b9e9e25151ea598490c196c777b2fd7e49be
Hadi Hamoud <hadihamoud@Hadis-MacBook-Pro.local>
2022-05-22 12:19:15 +0300

    moved

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, send_from_directory
 from flask import request
-from flask_cors import CORS
+#from flask_cors import CORS
 import requests
 import time
 import json
